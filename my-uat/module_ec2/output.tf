3dfaf906f21011ce2d0cfd2e5d169933778ed8a5
furrukhjaved <69966438+furrukhjaved@users.noreply.github.com>
2020-12-03 19:34:37 +0000

    updated output files and added main output.tf

--- my-uat/module_ec2/output.tf
+++ my-uat/module_ec2/output.tf
@@ -1,5 +1,7 @@
-/*
 output "ec2-data" {
-  value = aws_instance.mod-ec2.subnet_id
+  value = aws_instance.mod-ec2.get_password_data
 }
-*/
+
+output "ec2-getip" {
+  value = aws_instance.mod-ec2.private_ip
+}
---
+++ my-uat/output.tf
@@ -0,0 +1,7 @@
+output "ec2-data" {
+  value = module.ec2.ec2-data
+}
+
+output "ec2-getip" {
+  value = module.ec2.ec2-getip
+}
